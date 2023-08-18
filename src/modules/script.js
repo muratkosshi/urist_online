414adb382b6378b4d7061760e1fa62c3077a9313
murat.kosshi <murat.kosshi@gmail.com>
2023-08-18 17:38:39 +0500

    fix adapted for mobile

--- src/modules/script.js
+++ src/modules/script.js
@@ -62,4 +62,26 @@ $(document).ready(function () {
 window.addEventListener("resize", handleWindowResize);
 
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    // Получаем все ссылки в навигации
+    const navLinks = document.querySelectorAll('.nav_menu a');
+  
+    // Проходим по всем ссылкам и добавляем обработчик события на клик
+    navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault(); // Отменяем действие по умолчанию (переход по ссылке)
+        
+        const targetSectionId = link.getAttribute('href'); // Получаем ID целевой секции
+        const targetSection = document.querySelector(targetSectionId); // Получаем элемент целевой секции
+  
+        // Прокручиваем страницу до целевой секции
+        window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth' // Добавляем плавную анимацию
+        });
+  
+        // Применяем смещение к целевой секции
+        targetSection.style.transform = 'translateX(90px)'; // Измените смещение по вашим требованиям
+      });
+    });
+  });
