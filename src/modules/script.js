334b7840324e98018484b01669cb80de36e4d628
murat.kosshi <murat.kosshi@gmail.com>
2023-08-18 12:36:49 +0500

    fix adapted for mobile

--- src/modules/script.js
+++ src/modules/script.js
@@ -1,4 +1,30 @@
-
+$(document).ready(function () {
+    //мобильное меню
+    $('#mobile-menu-btn').click( function(event){
+        event.preventDefault();
+        $('#mobile-menu').fadeIn();
+        $('#mm__wrapper')
+            .css('visibility', 'visible')
+            .css('transform', 'translateX(0)');
+        $("body").css("overflow","hidden");
+    });
+    //закрытие мобильного меню
+    $('.mm__close').click( function(){
+        $('#mobile-menu').fadeOut();
+        $('#mm__wrapper')
+            .css('visibility', 'hidden')
+            .css('transform', 'translateX(-100%)');
+        $("body").css("overflow","auto");
+    });
+    $('.nav_menu a').click(function()
+    {
+        $('#mobile-menu').fadeOut();
+        $('#mm__wrapper')
+            .css('visibility', 'hidden')
+            .css('transform', 'translateX(-100%)');
+        $("body").css("overflow","auto");
+    })
+});
     $('.reviews_slider_block').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -10,7 +36,8 @@
             {
                 breakpoint: 1024,
                 settings:{
-                    slidesToShow:1
+                    slidesToShow:1,
+                    arrows: false,
                 }
             }
         ]
@@ -31,3 +58,5 @@
 
 // Добавляем обработчик события изменения размера окна
 window.addEventListener("resize", handleWindowResize);
+
+
